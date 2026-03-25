22581f1c480e6d68c128454587297d502d4e7a7f
nicolas7856 <carradorinicolas@gmail.com>
2026-03-26 00:22:47 +0100

    v1.2

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE = 'airchat-v2';
+const ASSETS = [
+  './', './index.html', './styles.css', './app.js', './manifest.json',
+  './lz-string.min.js', './qrious.min.js', './jsQR.js', './icon-192.png', './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
